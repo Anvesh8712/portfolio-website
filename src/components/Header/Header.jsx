dfdfa48f01448693c9f7aa7f0e62a558fe13ec57
Anvesh <atvanvesh@gmail.com>
2024-07-26 11:50:00 -0500

    fixed download resume

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,6 @@ import profile_pic from "../../assets/profile_pic.jfif";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Header = () => {
-  const onButtonClick = () => {
-    const pdfUrl = "Sunkara_Anvesh (5).pdf";
-    const link = document.createElement("a");
-    link.href = pdfUrl;
-    link.download = "Sunkara_Anvesh.pdf"; // specify the filename
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   return (
     <div className="header" id="home">
       <img src={profile_pic} alt="Profile"></img>
@@ -31,16 +21,14 @@ const Header = () => {
             Connect With Me
           </AnchorLink>
         </div>
-        {/* <a
-          id="download-link"
-          href="public/Sunkara_Anvesh (5).pdf"
+        <a
+          href="https://anvesh8712.github.io/portfolio-website/Sunkara_Anvesh%20(5).pdf"
           download="Sunkara_Anvesh.pdf"
           className="anchor-link"
-        > */}
-        <div onClick={onButtonClick} className="header-resume">
-          My Resume
-        </div>
-        {/* </a> */}
+          target="_blank"
+        >
+          <div className="header-resume">My Resume</div>
+        </a>
       </div>
     </div>
   );
