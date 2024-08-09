872adcbf1dd05f96bce0cf1e080c6d11318bdc71
Anvesh <atvanvesh@gmail.com>
2024-08-09 15:20:47 -0500

    updated projects

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,20 +1,65 @@
+// const Services_Data = [
+//   {
+//     s_no: "01",
+//     s_name: "Gym Tracker App, IOS app made with React Native",
+//     s_desc: `• Led the development of a gym tracking application using React Native, implementing Redux for advanced state management alongside Hooks for efficient UI state handling.
+//  \n• Designed and integrated the application with a robust database system for workout and progress tracking. Utilized PostgreSQL for efficient data storage and retrieval, ensuring seamless user data management.`,
+//     s_link: "https://github.com/Anvesh8712/React-Native-gym--tracker",
+//   },
+//   {
+//     s_no: "02",
+//     s_name: "NLP Model Development for Stock Price Prediction",
+//     s_desc: `• Engineered and evaluated a diverse range of NLP/ML models specifically for predicting stock price fluctuations based on financial articles. Applied Naive Bayes for initial tag predictions, Logistic Regression for deeper statistical analysis, CNNs for sequential data processing, and leveraged the BERT attention model to refine sentiment analysis, collectively enhancing prediction accuracy and market insights.
+//  \n• Implemented a custom web scraper to gather financial articles from Benzinga and integrated Yahoo Finance API for real-time stock price data to train and validate the models.`,
+//     s_link: "https://github.com/Anvesh8712/NLP-model-Evaluation",
+//   },
+//   {
+//     s_no: "03",
+//     s_name: "Website made with React and Spotify API",
+//     s_desc: `• Developed a single page, Tinder-like, music recommendation system using React and Spotify API, featuring an interactive ’swiping’ interface coupled with Spotify authentication for personalized song selection model.
+//  \n• Researched and emphasized UI/UX design principles to create an intuitive, responsive user experience; integrated a backend using Express.js and managed data efficiently with ElephantSQL.
+//  \n• Created wireframes and mockups on Figma to guide the development process, ensuring the final product was both aesthetically pleasing and functionally robust, with a focus on cross-browser compatibility.`,
+//     s_link: "https://github.com/Anvesh8712/spotify-tinder",
+//   },
+//   {
+//     s_no: "04",
+//     s_name: "AI Inventory Tracker With Gemini API",
+//     s_desc: `• Developed an inventory tracking application with user authentication and personalized inventory management.
+//  \n• Integrated with Firebase for authentication and Firestore for database management.
+//  \n• Utilized the Gemini API to suggest recipes based on the items available in the user's pantry, enhancing the app's functionality and user experience.
+//  \n• Deployed on Vercel for seamless and scalable hosting.`,
+
+//     s_link: "https://inventory-app-three-pink.vercel.app/",
+//     d_link: "https://github.com/Anvesh8712/inventory-app",
+//   },
+// ];
+
 const Services_Data = [
   {
     s_no: "01",
+    s_name:
+      "AI-Powered Chat Application, Next.js Web App with React, Firebase, and OpenAI API",
+    s_desc: `• Developed and deployed an AI-driven chat application using Next.js and React, leveraging the OpenAI API to provide dynamic and personalized conversations, hosted on an EC2 instance for scalable performance.
+ \n• Architected a secure backend with Firestore, enabling user-specific chat history storage and retrieval, and integrated Google Sign-In for streamlined authentication, enhancing security and user experience.
+ \n• Designed the application to allow users to create and interact with custom chatbots tailored to their own ideas or websites, providing a platform to visualize and test chatbot implementations in real-time.`,
+    s_link: "https://ai-chat-jade-nine.vercel.app/",
+  },
+  {
+    s_no: "02",
     s_name: "Gym Tracker App, IOS app made with React Native",
     s_desc: `• Led the development of a gym tracking application using React Native, implementing Redux for advanced state management alongside Hooks for efficient UI state handling.
  \n• Designed and integrated the application with a robust database system for workout and progress tracking. Utilized PostgreSQL for efficient data storage and retrieval, ensuring seamless user data management.`,
     s_link: "https://github.com/Anvesh8712/React-Native-gym--tracker",
   },
   {
-    s_no: "02",
+    s_no: "03",
     s_name: "NLP Model Development for Stock Price Prediction",
     s_desc: `• Engineered and evaluated a diverse range of NLP/ML models specifically for predicting stock price fluctuations based on financial articles. Applied Naive Bayes for initial tag predictions, Logistic Regression for deeper statistical analysis, CNNs for sequential data processing, and leveraged the BERT attention model to refine sentiment analysis, collectively enhancing prediction accuracy and market insights.
  \n• Implemented a custom web scraper to gather financial articles from Benzinga and integrated Yahoo Finance API for real-time stock price data to train and validate the models.`,
     s_link: "https://github.com/Anvesh8712/NLP-model-Evaluation",
   },
   {
-    s_no: "03",
+    s_no: "04",
     s_name: "Website made with React and Spotify API",
     s_desc: `• Developed a single page, Tinder-like, music recommendation system using React and Spotify API, featuring an interactive ’swiping’ interface coupled with Spotify authentication for personalized song selection model.
  \n• Researched and emphasized UI/UX design principles to create an intuitive, responsive user experience; integrated a backend using Express.js and managed data efficiently with ElephantSQL.
@@ -22,13 +67,12 @@ const Services_Data = [
     s_link: "https://github.com/Anvesh8712/spotify-tinder",
   },
   {
-    s_no: "04",
+    s_no: "05",
     s_name: "AI Inventory Tracker With Gemini API",
     s_desc: `• Developed an inventory tracking application with user authentication and personalized inventory management.
  \n• Integrated with Firebase for authentication and Firestore for database management.
  \n• Utilized the Gemini API to suggest recipes based on the items available in the user's pantry, enhancing the app's functionality and user experience.
  \n• Deployed on Vercel for seamless and scalable hosting.`,
-
     s_link: "https://inventory-app-three-pink.vercel.app/",
     d_link: "https://github.com/Anvesh8712/inventory-app",
   },
